3e0e800596ffed195d2eeeee7764e5edd69e3e25
pappugit11 <pappuram619@gmail.com>
2024-08-20 23:39:26 +0530

    ice-components

---
+++ ice-components/js/common.js
@@ -0,0 +1,243 @@
+jQuery(document).ready(function($){
+// document start
+
+
+ // Navbar
+ $( "<span class='clickD'><img src='images/drop-arrow.svg'></span>" ).insertAfter(".navbar-nav li.menu-item-has-children > a");
+ $('.navbar-nav li .clickD').click(function(e) {
+     e.preventDefault();
+     var $this = $(this);
+     if ($this.next().hasClass('show'))
+        {
+            $this.next().removeClass('show');
+            $this.removeClass('toggled');
+        } 
+        else 
+        {
+            $this.parent().parent().find('.sub-menu').removeClass('show');
+            $this.parent().parent().find('.toggled').removeClass('toggled');
+            $this.next().toggleClass('show');
+            $this.toggleClass('toggled');
+        }
+ });
+
+ $(window).on('resize', function(){
+     if ($(this).width() < 1025) {
+         $('html').click(function(){
+             $('.navbar-nav li .clickD').removeClass('toggled');
+             $('.toggled').removeClass('toggled');
+             $('.sub-menu').removeClass('show');
+         });
+         $(document).click(function(){
+             $('.navbar-nav li .clickD').removeClass('toggled');
+             $('.toggled').removeClass('toggled');
+             $('.sub-menu').removeClass('show');
+         });
+         $('.navbar-nav').click(function(e){
+            e.stopPropagation();
+         });
+     }
+ });
+ // Navbar end
+
+
+ 
+/* ===== For menu animation === */
+$(".navbar-toggler").click(function(){
+    $(".navbar-toggler").toggleClass("open");
+    $(".navbar-toggler .stick").toggleClass("open");
+    $('body,html' ).toggleClass("open-nav");
+});
+
+// Navbar end
+
+
+
+
+
+// to make sticky nav bar
+// $(window).scroll(function() {     
+//     var scroll = $(window).scrollTop();     
+//     if (scroll > 200) { 
+//         $(".main-head").addClass("fixed"); 
+//     } 
+//     else {
+//       $(".main-head").removeClass("fixed"); 
+//     }
+// }) 
+
+
+// smooth scroll to any section
+// if($('a.scroll').length){
+//     $("a.scroll").on('click', function(event) {
+//       if (this.hash !== "") {
+//         event.preventDefault();
+//         var target = this.hash, $target = $(target);
+//         $('html, body').animate({
+//           scrollTop: $target.offset().top - 60
+//         }, 800, function(){
+//           window.location.href.substr(0, window.location.href.indexOf('#'));
+//         });
+//       } 
+//     });
+  
+//   }
+
+
+// back to top
+if($("#scroll").length){
+    $(window).scroll(function(){ 
+        if ($(this).scrollTop() > 200) { 
+            $('#scroll').fadeIn(200); 
+        } else { 
+            $('#scroll').fadeOut(200); 
+        } 
+    }); 
+    $('#scroll').click(function(){ 
+        $("html, body").animate({ scrollTop: 0 }, 500); 
+        return false; 
+    }); 
+}
+
+
+
+
+// one page scroll menu link
+// $('a[href*="#"]').on('click', function (e) {
+//     e.preventDefault();
+//     $(document).off("scroll");
+//     $('.navbar-nav > li > a').each(function () {
+//         $(this).parent('li').removeClass('current-menu-item');
+//     });
+//     $(this).parent('li').addClass('current-menu-item');
+//     var target = this.hash, $target = $(target);
+//     $('html, body').stop().animate({
+//         'scrollTop': $target.offset().top
+//     }, 500, 'swing', function () {
+//         window.location.href.substr(0, window.location.href.indexOf('#'));
+//         $(document).on("scroll", onScroll);
+//     });
+// });
+//  $(document).on("scroll", onScroll);
+// function onScroll(event){
+//     var scrollPos = $(document).scrollTop() + 100;
+//     $('.navbar-nav > li > a').each(function () {
+//         var currLink = $(this);
+//         var refElement = $(currLink.attr("href"));
+//         if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+//             $('.navbar-nav > li').removeClass("current-menu-item");
+//             currLink.parent('li').addClass("current-menu-item");
+//         }
+//         else{
+//             currLink.parent('li').removeClass("current-menu-item");
+//         }
+//     });
+// }
+
+
+
+
+
+// slick slider
+
+$('.update-slider').slick({
+  dots: false,
+  arrows: true,
+  infinite: false,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: false,
+  autoplaySpeed: 3000,
+  centerMode: false,
+  adaptiveHeight: false,
+  centerPadding: '0px',
+  prevArrow: '.slider-prev',
+  nextArrow: '.slider-next',
+  responsive: [
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
+
+$(".srch-icon").click(function(){
+    $(".drop-form-wrap").toggleClass("form-show");
+});
+
+
+    //Horizontal Tab
+    $('#parentHorizontalTab').easyResponsiveTabs({
+        type: 'default', //Types: default, vertical, accordion
+        width: 'auto', //auto or any width like 600px
+        fit: true, // 100% fit in a container
+        tabidentify: 'hor_1', // The tab groups identifier
+        activate: function(event) { // Callback function if tab is switched
+            var $tab = $(this);
+            var $info = $('#nested-tabInfo');
+            var $name = $('span', $info);
+            $name.text($tab.text());
+            $info.show();
+        }
+    });
+
+    // Child Tab
+    $('#ChildVerticalTab_1').easyResponsiveTabs({
+        type: 'vertical',
+        width: 'auto',
+        fit: true,
+        tabidentify: 'ver_1', // The tab groups identifier
+        activetab_bg: '#fff', // background color for active tabs in this group
+        inactive_bg: '#fff', // background color for inactive tabs in this group
+        active_border_color: '#c1c1c1', // border color for active tabs heads in this group
+        active_content_border_color: '#5AB1D0' // border color for active tabs contect in this group so that it matches the tab head border
+    });
+
+    //Vertical Tab
+    $('#parentVerticalTab').easyResponsiveTabs({
+        type: 'vertical', //Types: default, vertical, accordion
+        width: 'auto', //auto or any width like 600px
+        fit: true, // 100% fit in a container
+        closed: 'accordion', // Start closed if in accordion view
+        tabidentify: 'hor_1', // The tab groups identifier
+        activate: function(event) { // Callback function if tab is switched
+            var $tab = $(this);
+            var $info = $('#nested-tabInfo2');
+            var $name = $('span', $info);
+            $name.text($tab.text());
+            $info.show();
+        }
+    });
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+// document end
+
+})
+
+
